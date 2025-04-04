ecd66728cdee06f0b96db3e9d2f244efc97c9d0b
EarthDLL <earthdll@outlook.com>
2025-04-04 14:34:39 +0800

    清理不需要的文件

---
+++ GDExtension/MineC/src/ChunkRenderData.cpp
@@ -0,0 +1,17 @@
+#include "ChunkRenderData.h"
+
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/classes/engine.hpp>
+#include <godot_cpp/classes/image.hpp>
+#include <godot_cpp/classes/array_mesh.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+#include <godot_cpp/variant/array.hpp>
+#include <godot_cpp/variant/variant.hpp>
+
+using namespace godot;
+
+
+void ChunkRenderData::_bind_methods() {
+    return;
+}
+
---
+++ GDExtension/MineC/src/ChunkRenderData.h
@@ -0,0 +1,44 @@
+#ifndef ChunkRenderData_H
+#define ChunkRenderData_H
+
+#include <bitset>
+#include <godot_cpp/classes/ref_counted.hpp>
+#include <godot_cpp/classes/noise.hpp>
+#include <godot_cpp/variant/variant.hpp>
+
+using namespace godot;
+
+class ChunkRenderData : public RefCounted{
+    GDCLASS(ChunkRenderData, RefCounted);
+
+private:
+    static void _bind_methods();
+public:
+    bool is_completed = false;
+
+    Vector2i chunk_id;
+    bool has_edge_north = false;
+    bool has_edge_south = false;
+    bool has_edge_west = false;
+    bool has_edge_east = false;
+    
+    Array main_data;
+    Array edge_data_north;
+    Array edge_data_south;
+    Array edge_data_west;
+    Array edge_data_east;
+    Array other_data;
+    
+    void set_main_data(Array data);
+    void set_north_data(Array data);
+    void set_south_data(Array data);
+    void set_west_data(Array data);
+    void set_east_data(Array data);
+    void set_other_data(Array data);
+    void set_id(Vector2i id);
+    
+    
+};
+
+
+#endif
